59c47932e89013cf37636eefde678ad6372c1e88
Roi <thedarkhermit@gmail.com>
2020-06-26 18:57:09 +0300

    Finished with comments

--- src/AbstractInvertedIndex.java
+++ src/AbstractInvertedIndex.java
@@ -1,40 +1,53 @@
 import java.io.File;
 import java.util.*;
 
+/**
+ * The abstract class for index systems
+ */
 public abstract class AbstractInvertedIndex {
     HashMap<String, TreeSet<String>> hashMap;
 
+    /**
+     * builds a dictionary that contains all words in the given documents list as keys 
+     * and the document names set that shoes where they were taken from as a value
+     * @param : lst- File[]  documents list
+     */
     public abstract void buildInvertedIndex(File[] lst);
 
+    /**
+     * gets the query and builds the output
+     * @param : str- the given query
+     * @return : a TreeSet contains the output for the current query
+     */
     public TreeSet runQuery(String str) {
         TreeSet<String> returnedValue = new TreeSet<String>();
 
-//        int returnValue = 0;
         String[] lineWords = str.split("\\W+");
         List<String> operands = Arrays.asList("OR", "AND", "NOT");
         Stack<String> stack = new Stack<String>();
 
-        for (String t : lineWords) {
-            if(this.getClass().getSimpleName()=="CaseInsensitiveIndex" && !operands.contains(t))
-            { t=t.toLowerCase();}
-            if (!operands.contains(t)) {
-                stack.push(t);
+        for (String word : lineWords) {
+            // if in s=case insensitive will set to lowercase
+            if(this.getClass().getSimpleName()=="CaseInsensitiveIndex" && !operands.contains(word))
+            { word=word.toLowerCase();}
+            if (!operands.contains(word)) {
+                stack.push(word);
             } else {
                 String a = stack.pop();
                 String b = stack.pop();
-                int index = operands.indexOf(t);
+                int index = operands.indexOf(word);
                 switch (index) {
-                    case 0:
+                    case 0://OR
                         returnedValue= OR_op(a, b);
                         stack.push(a+"or"+b+'*');
                         this.hashMap.put(a+"or"+b+'*',returnedValue);
                         break;
-                    case 1:
+                    case 1://AND
                         returnedValue = AND_op(a, b);
                         stack.push(a+"and"+b+'*');
                         this.hashMap.put(a+"and"+b+'*',returnedValue);
                         break;
-                    case 2:
+                    case 2://NOT
                         returnedValue=NOT_op(a, b);
                         stack.push(a+"NOT"+b+'*');
                         this.hashMap.put(a+"NOT"+b+'*',returnedValue);
@@ -43,13 +56,15 @@ public abstract class AbstractInvertedIndex {
             }
         }
 
-        while (!stack.empty()){
+        while (!stack.empty()){// deals with "word without operator " situations, adds all documents in such case
             returnedValue.addAll(this.hashMap.get(stack.pop()));
         }
 
         return returnedValue;
     }
-
+    /**
+     * @return : a TreeSet Contains all documents that contains a and all documents that contains b
+     */
     public TreeSet<String> OR_op(String a, String b) {
         TreeSet<String> relevant_docs = new TreeSet<String>();
         if (this.hashMap.containsKey(a)) {
@@ -60,6 +75,9 @@ public abstract class AbstractInvertedIndex {
         }
         return relevant_docs;
     }
+    /**
+     * @return : a TreeSet Contains  all documents that contains a and b
+     */
     public TreeSet<String> AND_op(String a, String b) {
         TreeSet<String> relevant_docs = new TreeSet<String>();
         TreeSet<String> tree_a =  this.hashMap.get(a);
@@ -71,6 +89,9 @@ public abstract class AbstractInvertedIndex {
         }
         return relevant_docs;
     }
+    /**
+     * @return : a TreeSet Contains  all documents that contains a and NOT b
+     */
     public TreeSet<String> NOT_op(String a, String b) {
         TreeSet<String> relevant_docs = new TreeSet<String>();
         TreeSet<String> tree_a = (TreeSet<String>) this.hashMap.get(a);
